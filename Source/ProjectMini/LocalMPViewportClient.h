84e4daa85f9622c7524066889ff28f2704c23875
Brandon Jones <bcjones1@fullsail.edu>
2018-03-05 18:21:02 -0500

    Adding Source

---
+++ Source/ProjectMini/LocalMPViewportClient.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameViewportClient.h"
+#include "LocalMPViewportClient.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTMINI_API ULocalMPViewportClient : public UGameViewportClient
+{
+	GENERATED_BODY()
+	
+	
+	virtual bool InputKey(FViewport* Viewport, int32 ControllerId, FKey Key, EInputEvent EventType, float AmountDepressed = 1.f, bool bGamepad = false) override;
+	virtual bool InputAxis(FViewport* Viewport, int32 ControllerId, FKey Key, float Delta, float DeltaTime, int32 NumSamples = 1, bool bGamepad = false) override;
+
+
+	
+};
